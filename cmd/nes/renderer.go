ca8b2787d0add0ef8ffc08b0b51a8134ff9aabed
thara <git@thara.dev>
2022-05-01 16:17:05 +0900

    :bug: Fix pixels update

--- cmd/nes/renderer.go
+++ cmd/nes/renderer.go
@@ -9,33 +9,37 @@ import (
 )
 
 type renderer struct {
-	buf *bytes.Buffer
-	px  []byte
+	px []byte
 }
 
 func newRenderer() *renderer {
 	return &renderer{
-		buf: bytes.NewBuffer(make([]byte, 4*ppu.WIDTH*ppu.HEIGHT)),
-		px:  make([]byte, 4*ppu.WIDTH*ppu.HEIGHT),
+		px: make([]byte, 4*ppu.WIDTH*ppu.HEIGHT),
 	}
 }
 
 func (r *renderer) UpdateFrame(buf *[ppu.WIDTH * ppu.HEIGHT]uint8) {
-	p := make([]uint32, len(buf))
 	for i, v := range buf {
-		p[i] = palletes[v]
+		r.px[i] = palettes[v]
+		r.px[i+1] = palettes[v+1]
+		r.px[i+2] = palettes[v+2]
+		r.px[i+3] = palettes[v+3]
 	}
-	if err := binary.Write(r.buf, binary.BigEndian, p); err != nil {
-		log.Fatalf("fail to write video buffer: %v", err)
-	}
-	r.px = r.buf.Bytes()
 }
 
 func (r *renderer) pixels() []byte {
 	return r.px
 }
 
-var palletes []uint32 = []uint32{
+var palettes []byte = func() []byte {
+	var bf bytes.Buffer
+	if err := binary.Write(&bf, binary.BigEndian, rgba); err != nil {
+		log.Fatalf("fail to write video buffer: %v", err)
+	}
+	return bf.Bytes()
+}()
+
+var rgba []uint32 = []uint32{
 	0x7C7C7CFF, 0x0000FCFF, 0x0000BCFF, 0x4428BCFF, 0x940084FF, 0xA80020FF, 0xA81000FF, 0x881400FF,
 	0x503000FF, 0x007800FF, 0x006800FF, 0x005800FF, 0x004058FF, 0x000000FF, 0x000000FF, 0x000000FF,
 	0xBCBCBCFF, 0x0078F8FF, 0x0058F8FF, 0x6844FCFF, 0xD800CCFF, 0xE40058FF, 0xF83800FF, 0xE45C10FF,

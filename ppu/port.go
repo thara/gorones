f4c93c2c687f324144d042a1a7a9a9f59091889c
thara <git@thara.dev>
2022-05-01 10:47:37 +0900

    :muscle: Update scrolling by register

--- ppu/port.go
+++ ppu/port.go
@@ -49,11 +49,40 @@ func (p *Port) WriteRegister(addr uint16, value uint8) {
 	case 0x2005:
 		// http://wiki.nesdev.org/w/index.php/PPU_scrolling#.242005_first_write_.28w_is_0.29
 		// http://wiki.nesdev.org/w/index.php/PPU_scrolling#.242005_second_write_.28w_is_1.29
-		//TODO
+		d := uint16(value)
+		if !p.ppu.w {
+			// first write
+			// t: ....... ...HGFED = d: HGFED...
+			// x:              CBA = d: .....CBA
+			p.ppu.t &^= 0b11111
+			p.ppu.t |= (d & 0b11111000) >> 3
+			p.ppu.x = value & 0b111
+		} else {
+			// second write
+			// t: CBA..HG FED..... = d: HGFEDCBA
+			p.ppu.t &^= 0b111001111100000
+			p.ppu.t |= ((d & 0b111) << 12) | ((d & 0b11111000) << 2)
+		}
+		p.ppu.w = !p.ppu.w
 	case 0x2006:
 		// http://wiki.nesdev.org/w/index.php/PPU_scrolling#.242006_first_write_.28w_is_0.29
 		// http://wiki.nesdev.org/w/index.php/PPU_scrolling#.242006_second_write_.28w_is_1.29
-		//TODO
+		d := uint16(value)
+		if !p.ppu.w {
+			// first write
+			// t: .FEDCBA ........ = d: ..FEDCBA
+			// t: X...... ........ = 0
+			p.ppu.t &^= 0b011111100000000
+			p.ppu.t |= (d & 0b11111) << 8
+		} else {
+			// second write
+			// t: ....... HGFEDCBA = d: HGFEDCBA
+			// v                   = t
+			p.ppu.t &^= 0b11111111
+			p.ppu.t |= d
+			p.ppu.v = p.ppu.t
+		}
+		p.ppu.w = !p.ppu.w
 	case 0x2007:
 		p.ppu.write(p.ppu.v, value)
 	}

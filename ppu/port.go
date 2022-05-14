f648940439ce21e3aed0e0045e15b13c02e82971
thara <git@thara.dev>
2022-05-14 21:52:54 +0900

    :hammer: Refactor ppu port

--- ppu/port.go
+++ ppu/port.go
@@ -13,7 +13,7 @@ func (p *Port) ReadRegister(addr uint16) uint8 {
 		p.ppu.w = false
 		// race condition
 		if p.ppu.scan.line == 241 && p.ppu.scan.dot < 2 {
-			result ^= 0x80
+			result &^= 0x80
 		}
 	case 0x2004: // OAMDATA
 		// https://wiki.nesdev.com/w/index.php/PPU_sprite_evaluation
@@ -31,11 +31,7 @@ func (p *Port) ReadRegister(addr uint16) uint8 {
 		} else {
 			result = p.ppu.read(p.ppu.v)
 		}
-		if p.ppu.ctrl.vramIncr {
-			p.ppu.v += 32
-		} else {
-			p.ppu.v += 1
-		}
+		incrV(p.ppu)
 	default:
 		result = p.ppu.cpuDataBus
 	}
@@ -64,14 +60,12 @@ func (p *Port) WriteRegister(addr uint16, value uint8) {
 			// first write
 			// t: ....... ...HGFED = d: HGFED...
 			// x:              CBA = d: .....CBA
-			p.ppu.t &^= 0b11111
-			p.ppu.t |= (d & 0b11111000) >> 3
+			p.ppu.t = (p.ppu.t &^ 0b11111) | (d&0b11111000)>>3
 			p.ppu.x = value & 0b111
 		} else {
 			// second write
 			// t: CBA..HG FED..... = d: HGFEDCBA
-			p.ppu.t &^= 0b111001111100000
-			p.ppu.t |= ((d & 0b111) << 12) | ((d & 0b11111000) << 2)
+			p.ppu.t = (p.ppu.t &^ 0b111001111100000) | ((d & 0b111) << 12) | ((d & 0b11111000) << 2)
 		}
 		p.ppu.w = !p.ppu.w
 	case 0x2006:
@@ -82,23 +76,25 @@ func (p *Port) WriteRegister(addr uint16, value uint8) {
 			// first write
 			// t: .FEDCBA ........ = d: ..FEDCBA
 			// t: X...... ........ = 0
-			p.ppu.t &^= 0b011111100000000
-			p.ppu.t |= (d & 0b111111) << 8
+			p.ppu.t = (p.ppu.t &^ 0b011111100000000) | ((d & 0b111111) << 8)
 		} else {
 			// second write
 			// t: ....... HGFEDCBA = d: HGFEDCBA
 			// v                   = t
-			p.ppu.t &^= 0b11111111
-			p.ppu.t |= d
+			p.ppu.t = (p.ppu.t &^ 0b11111111) | d
 			p.ppu.v = p.ppu.t
 		}
 		p.ppu.w = !p.ppu.w
 	case 0x2007:
 		p.ppu.write(p.ppu.v, value)
-		if p.ppu.ctrl.vramIncr {
-			p.ppu.v += 32
-		} else {
-			p.ppu.v += 1
-		}
+		incrV(p.ppu)
+	}
+}
+
+func incrV(p *PPU) {
+	if p.ctrl.vramIncr {
+		p.v += 32
+	} else {
+		p.v += 1
 	}
 }

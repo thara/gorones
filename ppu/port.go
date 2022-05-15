4b99cc7d528aae20c61c6652d463d630ebe818db
thara <git@thara.dev>
2022-05-15 21:19:34 +0900

    :memo: Add PPU register comment

--- ppu/port.go
+++ ppu/port.go
@@ -23,7 +23,7 @@ func (p *Port) ReadRegister(addr uint16) uint8 {
 		} else {
 			result = p.ppu.spr.oam[p.ppu.oamAddr]
 		}
-	case 0x2007:
+	case 0x2007: // PPUDATA
 		// https://www.nesdev.org/wiki/PPU_registers#The_PPUDATA_read_buffer_(post-fetch)
 		if p.ppu.v <= 0x3EFF {
 			result = p.ppu.data
@@ -41,18 +41,18 @@ func (p *Port) ReadRegister(addr uint16) uint8 {
 
 func (p *Port) WriteRegister(addr uint16, value uint8) {
 	switch addr {
-	case 0x2000:
+	case 0x2000: // PPUCTRL
 		p.ppu.setController(value)
 		// t: ...BA.. ........ = d: ......BA
 		p.ppu.t = p.ppu.t&^uint16(0b110000000000) | uint16(p.ppu.ctrl.nt)<<10
-	case 0x2001:
+	case 0x2001: // PPUMASK
 		p.ppu.setMask(value)
-	case 0x2003:
+	case 0x2003: // OAMADDR
 		p.ppu.oamAddr = value
-	case 0x2004:
+	case 0x2004: // OAMDATA
 		p.ppu.spr.oam[p.ppu.oamAddr] = value
 		p.ppu.oamAddr++
-	case 0x2005:
+	case 0x2005: // PPUSCROLL
 		// http://wiki.nesdev.org/w/index.php/PPU_scrolling#.242005_first_write_.28w_is_0.29
 		// http://wiki.nesdev.org/w/index.php/PPU_scrolling#.242005_second_write_.28w_is_1.29
 		d := uint16(value)
@@ -68,7 +68,7 @@ func (p *Port) WriteRegister(addr uint16, value uint8) {
 			p.ppu.t = (p.ppu.t &^ 0b111001111100000) | ((d & 0b111) << 12) | ((d & 0b11111000) << 2)
 		}
 		p.ppu.w = !p.ppu.w
-	case 0x2006:
+	case 0x2006: // PPUADDR
 		// http://wiki.nesdev.org/w/index.php/PPU_scrolling#.242006_first_write_.28w_is_0.29
 		// http://wiki.nesdev.org/w/index.php/PPU_scrolling#.242006_second_write_.28w_is_1.29
 		d := uint16(value)
@@ -85,7 +85,7 @@ func (p *Port) WriteRegister(addr uint16, value uint8) {
 			p.ppu.v = p.ppu.t
 		}
 		p.ppu.w = !p.ppu.w
-	case 0x2007:
+	case 0x2007: // PPUDATA
 		p.ppu.write(p.ppu.v, value)
 		incrV(p.ppu)
 	}
